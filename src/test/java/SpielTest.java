1b510f23f089ff812f4b14e02b30c4a32f551cd4
Daniel Töpel <toepel@gmx.it>
2025-06-30 10:14:56 +0200

    init

---
+++ src/test/java/SpielTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpielTest {
+    @Test
+    public void checkX0() {
+        assertEquals(0, PlayerCharacter.getX());
+    }
+
+}
